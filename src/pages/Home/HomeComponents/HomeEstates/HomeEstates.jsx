6d47a6a6a074a96ad7d97c1c26041f99b7c0aa90
Amin7775 <amin.dpi24@gmail.com>
2024-04-14 11:51:12 +0600

    Fixed responsiveness for home estates section

--- src/pages/Home/HomeComponents/HomeEstates/HomeEstates.jsx
+++ src/pages/Home/HomeComponents/HomeEstates/HomeEstates.jsx
@@ -12,22 +12,22 @@ const HomeEstates = () => {
         .then(data => setEstates(data))
     },[])
   return (
-    <div className="bg-[#F4EDE8] pt-32 pb-28 mt-32">
+    <div className="bg-[#F4EDE8] pt-24 lg:pt-32 pb-16 md:pb-28 mt-32">
       {/* container */}
-      <div className="w-[95%] mx-auto">
+      <div className="w-[90%] md:w-[95%] mx-auto">
         {/* text content */}
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">New Listings</h1>
-        <p className="mt-y md:mt-2 lg:mt-4 text-xl md:text-2xl   text-slate-500">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center lg:text-start">New Listings</h1>
+        <p className="mt-1 md:mt-2 lg:mt-4 text-lg md:text-2xl  text-center lg:text-start text-slate-500">
           Explore new & featured properties for sale
         </p>
         {/* cards */}
-        <div className="grid grid-cols-4 gap-14 mt-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8  lg:gap-14 mt-10 lg:mt-20">
             {
                 estates?.slice(0,8).map(estate => <EstatesCard key={estate.id} estate={estate}></EstatesCard>)
             }
         </div>
         {/* view all btn */}
-        <div className="flex justify-center mt-14">
+        <div className="flex justify-center mt-8 md:mt-14">
             <Link to={'/estates'}>
             <button className="bg-custom-text-1 text-white hover:bg-black tra px-8 py-3 h-full rounded-xl text-xl ease-linear duration-200">
                 View All
